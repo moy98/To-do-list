d7520ee447234c9e84a6fa62efa7e9279db7f62c
moises medina <moi98.medina@gmail.com>
2022-07-22 13:50:22 -0500

    clase03 terminada

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const btn=document.querySelector('[data-form-btn]');
+const btn = document.querySelector('[data-form-btn]');
 
 //Funcion anonima
-const createTask=(evento) => {
- evento.preventDefault();
- const input=document.querySelector('[data-form-input]');
- console.log(input.value);
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value
+    input.value = ''
+    const list = document.querySelector('[data-list]')
+    const task = document.createElement('li')
+    task.classList.add('card')
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div><i class="fas fa-trash-alt trashIcon icon"></i> `
+    task.innerHTML = content;
+    console.log(task);
+    list.appendChild(task);
 };
 console.log(btn);
 // Arrow functionsofunciones anonimas
-btn.addEventListener('click',createTask);
+btn.addEventListener('click', createTask);
